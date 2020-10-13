b7d4e06ede5277cdb279fe76f3393b0fd9920ca9
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-12 22:00:36 -0400

    Basic project files.

---
+++ routes/snippets.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const Snippet = require('../models/snippet')
+const router = express.Router()
+
+router.get('/new', (req, res) => {
+  res.render('snippet/new', { snippet: new Snippet() })
+})
+
+// router.get('/edit/:id', async (req, res) => {
+//   const article = await Article.findById(req.params.id)
+//   res.render('snippet/edit', { article: article })
+// })
+
+router.get('/:id', async (req, res) => {
+  paste  = await Snippet.findById(req.params.id)
+  res.render('snippet/show', { snippet: paste })
+})
+
+router.post('/', async (req, res, next) => {
+  req.snippet = new Snippet()
+  next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+  req.snippet = await Snippet.findById(req.params.id)
+  next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+  await Snippet.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let snippet = req.snippet
+    snippet.title = req.body.title
+    snippet.description = req.body.description
+    snippet.code = req.body.code
+    try {
+      snippet = await snippet.save()
+      res.redirect(`/snippet/${snippet.id}`)
+    } catch (e) {
+      res.render(`snippet/${path}`, { snippet: snippet })
+    }
+  }
+}
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Snippet = require('./models/snippet')
+const snippetRouter = require('./routes/snippets')
+const methodOverride = require('method-override')
+const app = express()
+
+const mongoconnection = require('./mongoclient.json')
+
+mongoose.connect(mongoconnection.mongoconnectionid, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true  }, () => console.log('Connected to DB!'))
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const snippets = await Snippet.find().sort({ createdAt: 'desc' })
+  res.render('snippet/index', { snippets: snippets })
+})
+
+app.use('/snippet', snippetRouter)
+
+app.listen(5000)
