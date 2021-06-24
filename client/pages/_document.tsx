50eab0b27b5289c81c9b25a30d6ea2fc08d02f44
Harsh Singh <hi.harsh@protonmail.ch>
2021-06-24 11:21:34 -0400

    Removed old stitches file

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { getCssString } from './../stitches.config'
+import { getCssString } from '@css/theme.config'
 
 export default class Document extends NextDocument {
   render() {
