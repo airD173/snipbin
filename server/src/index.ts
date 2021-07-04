d1736ba4bc6dbc156879c1b549ec843f822ff903
Harsh SIngh <hi.harsh@protonmail.ch>
2021-07-04 01:05:24 -0400

    Removed Next.js backend configuration from Heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,4 @@ dotenv.config()
 
 import { StartServer } from './server.js'
 
-import next from 'next'
-const app = next({ dev: true })
-
-app.prepare().then(() => {
-    StartServer()
-})
+StartServer()
