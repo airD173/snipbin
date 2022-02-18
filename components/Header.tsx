41c160169057ce2261221a108df3b21ef5caff3e
Harsh Singh <hi.harsh@protonmail.ch>
2022-02-18 15:02:32 -0500

    Fixed header tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ of other PasteBins.'
 of other PasteBins.'
       />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+      <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
       <meta name='theme-color' content='#ffffff' />
       <link
         rel='icon'
